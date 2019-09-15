09055c17699e05bc799f835e71feb34f87db4175
jseg787 <jsegarra3@gmail.com>
2019-09-15 15:49:36 -0400

    random numbers for pokemon are now unique

--- src/App.js
+++ src/App.js
@@ -1,26 +1,113 @@
 import React from 'react';
-import logo from './logo.svg';
+import pokeballicon from './pokeball-icon.svg';
 import './App.css';
 
-function App() {
+class App extends React.Component {
+
+  render() {
+    return (
+      <div className="App">
+        <Header />
+        <Game />
+      </div>
+    );
+  }
+}
+
+const Header = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <header className="text-center">
+      <h1>Pokemon Guesser</h1>
+    </header>
+  );
+}
+
+class Game extends React.Component {
+  state = {
+    score: 0
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <PokemonArea />
+      </div>
+    );
+  }
+}
+
+class PokemonArea extends React.Component {
+  state = {
+    pokemon: []
+  };
+
+  componentDidMount = () => {
+    this.getPokemon();
+  }
+
+  getPokemon = async () => {
+    const Pokedex = require('pokeapi-js-wrapper');
+    const P = new Pokedex.Pokedex();
+    const pokeNumArr = this.randNumGen();
+    const pokeArr = [];
+    for (let num of pokeNumArr) {
+      const pokeHolder = await P.resource(`/api/v2/pokemon/${num}/`);
+      pokeArr.push(pokeHolder);
+    }
+
+    this.setState({ pokemon: [...pokeArr] });
+  };
+
+  randNumGen = () => {
+    const nums = [];
+
+    const prevNums = [];
+    while (nums.length < 4) {
+      let currNum = Math.ceil(Math.random() * 151);
+      if (!prevNums.includes(currNum)) {
+        nums.push(currNum);
+        prevNums.push(currNum);
+      }
+    }
+    return nums;
+  }
+
+  render() {
+    const pokemon = this.state.pokemon.map(poke => {
+      return (
+        <div className="p-image-card col-6" /* onClick={makeChoice} */>
+          <img
+            className="p-image"
+            src={poke.sprites.front_default}
+            alt=""
+            key={`pokemon-${poke.id}`}
+          />
+        </div>
+      );
+    });
+
+    return (
+      <div>
+        {pokemon.length > 0 ? (
+          <div className="row">
+            {pokemon}
+          </div>
+        ) : (
+            <PokeballSpinner />
+          )}
+      </div>
+    );
+  }
+}
+
+const PokeballSpinner = () => {
+  return (
+    <div className="text-center">
+      <img className="block App-logo" src={pokeballicon} alt="pokeball-icon" />
+      <p><a href="http://www.onlinewebfonts.com">oNline Web Fonts</a></p>
     </div>
   );
 }
+
 
 export default App;
