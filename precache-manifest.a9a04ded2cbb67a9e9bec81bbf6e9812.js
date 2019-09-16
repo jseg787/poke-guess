dc46cc5bdc4dd948207b84565dbbf227ae041316
jseg787 <jsegarra3@gmail.com>
2019-09-16 12:44:01 -0400

    Updates

---
+++ precache-manifest.a9a04ded2cbb67a9e9bec81bbf6e9812.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c6902ee5fff38d6ba10242e8c0bcc53",
+    "url": "/poke-guess/index.html"
+  },
+  {
+    "revision": "7898fb9de4fc58d36cc5",
+    "url": "/poke-guess/static/css/main.c2a74bc4.chunk.css"
+  },
+  {
+    "revision": "5242810e1e0ee575fbc0",
+    "url": "/poke-guess/static/js/2.7ad8ccf6.chunk.js"
+  },
+  {
+    "revision": "7898fb9de4fc58d36cc5",
+    "url": "/poke-guess/static/js/main.ab08c5fa.chunk.js"
+  },
+  {
+    "revision": "8db5ec0f1d92a80cc27e",
+    "url": "/poke-guess/static/js/runtime~main.f7229598.js"
+  },
+  {
+    "revision": "67d3cdb85f20f089ac72b62636880456",
+    "url": "/poke-guess/static/media/pokeball-icon.67d3cdb8.svg"
+  }
+]);
