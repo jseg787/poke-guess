ba662147a691e1fe7f6f0f21b5b4a22f6dedc9e0
jseg787 <jsegarra3@gmail.com>
2019-09-28 16:18:31 -0400

    Switch from stateful class components to hooks

--- src/App.js
+++ src/App.js
@@ -1,19 +1,15 @@
-import React from 'react';
-import pokeballicon from './pokeball-icon.svg';
-import './App.css';
-import { get } from 'https';
+import React, { useState, useEffect } from "react";
+import pokeballicon from "./pokeball-icon.svg";
+import "./App.scss";
 
-class App extends React.Component {
-
-  render() {
-    return (
-      <div className="App">
-        <Header />
-        <Game />
-      </div>
-    );
-  }
-}
+const App = () => {
+  return (
+    <div className="App">
+      <Header />
+      <Game />
+    </div>
+  );
+};
 
 const Header = () => {
   return (
@@ -21,35 +17,42 @@ const Header = () => {
       <h1>Pokemon Guesser</h1>
     </header>
   );
-}
+};
 
-class Game extends React.Component {
-  state = {
-    score: 0,
-    pokemon: [],
-    selectedPokemon: {}
-  }
-  
-  getPokemon = async () => {
-    const Pokedex = require('pokeapi-js-wrapper');
+const Game = () => {
+
+  const [score, setScore] = useState(0);
+  const [pokemon, setPokemon] = useState([]);
+  const [selectedPokemon, setSelectedPokemon] = useState({});
+  // const [gameState, setGameState] = useState(0);
+
+  useEffect(() => {
+    getPokemon();
+  }, [score]);
+
+  const getPokemon = async () => {
+    const Pokedex = require("pokeapi-js-wrapper");
     const P = new Pokedex.Pokedex();
-    const pokeNumArr = this.randNumGen();
+    const pokeNumArr = randNumGen();
     const pokeArr = [];
     for (let num of pokeNumArr) {
       const pokeHolder = await P.resource(`/api/v2/pokemon/${num}/`);
       pokeArr.push(pokeHolder);
     }
-  
-    this.setState({ 
-      pokemon: pokeArr,
-      selectedPokemon: pokeArr[Math.floor(Math.random() * 4)]
-    });
+
+    // this.setState({
+    //   pokemon: pokeArr,
+    //   selectedPokemon: pokeArr[Math.floor(Math.random() * 4)]
+    // });
+
+    setPokemon(pokeArr);
+    setSelectedPokemon(pokeArr[Math.floor(Math.random() * 4)]);
   };
 
-  randNumGen = () => {
+  const randNumGen = () => {
     const nums = [];
-  
     const prevNums = [];
+
     while (nums.length < 4) {
       let currNum = Math.ceil(Math.random() * 151);
       if (!prevNums.includes(currNum)) {
@@ -57,49 +60,44 @@ class Game extends React.Component {
         prevNums.push(currNum);
       }
     }
+
     return nums;
-  }
+  };
 
-  componentDidMount = () => {
-    this.getPokemon();
-  }
-
-  handleClick = (e) => {
-    if (e.target.firstChild.src === this.state.selectedPokemon.sprites.front_default) {
-      console.log('correct');
-      this.setState({score: this.state.score + 1});
+  const handleClick = e => {
+    if (e.target.firstChild.src === selectedPokemon.sprites.front_default) {
+      console.log("correct");
+      // this.setState({ score: this.state.score + 1 });
+      setScore(score + 1);
     } else {
-      console.log('incorrect');
+      console.log("incorrect");
     }
-    setTimeout(this.getPokemon, 100);
-  }
-  
-  render() {
-    return (
-      <div className="container">
-        <GameInfo score={this.state.score}/>
-        <PokemonArea 
-        pokemon={this.state.pokemon} 
-        selected={this.state.selectedPokemon}
-        handleClick={this.handleClick}
-      />
-      </div>
-    );
-  }
-}
+    // setTimeout(this.getPokemon, 100);
+  };
 
-const PokemonArea = (props) => {
+  return (
+    <div className="container">
+      <GameInfo score={score} />
+      <PokemonArea
+        pokemon={pokemon}
+        selected={selectedPokemon}
+        handleClick={handleClick}
+      />
+    </div>
+  );
+};
+
+const PokemonArea = props => {
   const pokemon = props.pokemon.map(poke => {
     return (
-      <div 
-        className="p-image-card my-3" 
-        key={`pokemon-${poke.id}`}
-        onClick={props.handleClick} >
-        <img
-          className="p-image"
-          src={poke.sprites.front_default}
-          alt=""
-        />
+      <div className="col-6 col-md-3 my-3">
+        <div
+          className="p-image-card"
+          key={`pokemon-${poke.id}`}
+          onClick={props.handleClick}
+        >
+          <img className="p-image" src={poke.sprites.front_default} alt="" />
+        </div>
       </div>
     );
   });
@@ -108,37 +106,37 @@ const PokemonArea = (props) => {
     <div>
       {pokemon.length > 0 ? (
         <div>
-        <PokemonName selected={props.selected} />
-        <div className="row">
-          {pokemon}
-        </div>
+          <PokemonName selected={props.selected} />
+          <div className="row">{pokemon}</div>
         </div>
       ) : (
         <PokeballSpinner />
       )}
     </div>
   );
-}
+};
 
-const GameInfo = (props) => {
-  return (
-    <h1>{props.score}</h1>
-  )
-}
+const GameInfo = props => {
+  return <h1>{props.score}</h1>;
+};
 
-const PokemonName = (props) => {
+const PokemonName = props => {
   return (
-    <h2 className="selected-poke-name" style={{textAlign: 'center'}}>{props.selected.name}</h2>
-  )
-}
+    <h2 className="selected-poke-name" style={{ textAlign: "center" }}>
+      {props.selected.name}
+    </h2>
+  );
+};
 
 const PokeballSpinner = () => {
   return (
     <div className="text-center">
       <img className="block App-logo" src={pokeballicon} alt="pokeball-icon" />
-      <p><a href="http://www.onlinewebfonts.com">oNline Web Fonts</a></p>
+      <p>
+        <a href="http://www.onlinewebfonts.com">oNline Web Fonts</a>
+      </p>
     </div>
   );
-}
+};
 
 export default App;
